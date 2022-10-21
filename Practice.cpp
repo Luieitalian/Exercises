a26d1a88ff819ade8e4eae050786c56fea21ecf8
Luieitalian <enderbud125@hotmail.com>
2022-10-22 01:24:32 +0300

    TTT

--- Practice.cpp
+++ Practice.cpp
@@ -4,12 +4,29 @@
 #include <ios>
 #include <cmath>
 #include <clocale>
+#include <typeinfo>
 using namespace std;
 
 int main (){
     setlocale(LC_ALL , "Turkish");
+    int getMatrisSatirSayisi;
+    int getMatrisSütunSayisi;
+
+    int a = 2;
+    auto mk = typeid(a).name();
+    cout << typeid(mk).name();
+    //cout << "Matrisin satır sayısını giriniz : ";
+    //cin >> getMatrisSatirSayisi;
+
+    //cout << "Matrisin sütun sayısını giriniz : ";
+    //cin >> getMatrisSütunSayisi;
+
+
+
+    //int matris[getMatrisSatirSayisi][getMatrisSütunSayisi];
     
-    cout << "Im sooooooo confused";
+
+
 
     return 0;
 }
