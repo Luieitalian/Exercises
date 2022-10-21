c56914113f9b2d1ec337982fad9f8da6f32d6d0d
Luieitalian <116390699+Luieitalian@users.noreply.github.com>
2022-10-22 00:47:24 +0300

    Add files via upload

---
+++ Practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <ios>
+#include <cmath>
+#include <clocale>
+using namespace std;
+
+int main (){
+    setlocale(LC_ALL , "Turkish");
+    
+    cout << "Im confused";
+
+    return 0;
+}
