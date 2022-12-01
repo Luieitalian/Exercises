44dfba243693b625242f13e3e6330a02ddf32e20
Luieitalian <enderbud125@hotmail.com>
2022-12-01 17:02:03 +0300

    Updated Matrice

--- Matris.cpp
+++ Matris.cpp
@@ -3,40 +3,70 @@
 #include <iomanip>
 #include <ios>
 #include <cmath>
-#include <clocale>
+#include <locale.h>
 #include <typeinfo>
+#include <cstdlib>
 using namespace std;
 
-int main (){
-    setlocale(LC_ALL , "Turkish");
-    int getMatrisSatirSayisi;
-    int getMatrisSütunSayisi;
+/*
+    TR:
+    Matris boyutu kullanıcıdan istenir, girilen boyutlarda bir tam sayı matrisi oluşturulur.
+    Matris elemanları kullanıcıdan istenir, sonrasında matris ekrana yazdırılır.
+
+    EN:
+    Gets the size of the matrice from the user and creates one according to the input from the user.
+    Gets the elements of the matrice from the user and prints the matrice on the screen.
+*/
+bool isInt(string number)   // Checks the input, if there's any non-digit char, returns false.
+{
+
+    for (int i = 0; i < number.length(); i++)
+    {
+        if (isdigit(number[i]) == false)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+
+    setlocale(LC_ALL, "Turkish.UTF8");
+    string matriceRow;
+    string matriceColumn;
 
     cout << "Matrisin satır sayısını giriniz : ";
-    cin >> getMatrisSatirSayisi;
+    getline(cin, matriceRow);
     cout << "Matrisin sütun sayısını giriniz : ";
-    cin >> getMatrisSütunSayisi;
+    getline(cin, matriceColumn);
 
-    int matris[getMatrisSatirSayisi][getMatrisSütunSayisi];
-    
-    for (int i = 0; i < getMatrisSatirSayisi; i++)
+    while ((isInt(matriceRow) && isInt(matriceColumn)) == false)
     {
-        for (int j = 0; j < getMatrisSütunSayisi; j++)
+        cout << "Lütfen Geçerli Değerler Giriniz!" << endl;
+        cout << "Matrisin satır sayısını giriniz : ";
+        getline(cin, matriceRow);
+        cout << "Matrisin sütun sayısını giriniz : ";
+        getline(cin, matriceColumn);
+    }
+
+    int matris[stoi(matriceRow)][stoi(matriceColumn)]{0};
+
+    for (int i = 0; i < stoi(matriceRow); i++)
+    {
+        for (int j = 0; j < stoi(matriceColumn); j++)
         {
             cout << "Matrisin " << i + 1 << ". satırında " << j + 1 << ". sütunundaki sayıyı giriniz : ";
             cin >> matris[i][j];
         }
     }
-    for (int p = 0; p < getMatrisSatirSayisi; p++)
+    for (int p = 0; p < stoi(matriceRow); p++)
     {
-        for (int k = 0; k < getMatrisSütunSayisi; k++)
+        for (int k = 0; k < stoi(matriceColumn); k++)
         {
             cout << matris[p][k] << " ";
         }
         cout << endl;
     }
-    
-
 
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# General
+
+Exercises for learning C++
